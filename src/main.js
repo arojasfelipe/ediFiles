26701db325463796cbff56ad20d8b126cb8ddccf
unknown <you@example.com>
2022-05-06 08:34:54 -0500

    iniital

---
+++ src/main.js
@@ -0,0 +1,147 @@
+import { render } from 'react-dom';
+import './index.css';
+import * as React from 'react';
+import { SampleBase } from './sample-base';
+import {
+    DocumentEditorContainerComponent,
+    Toolbar
+} from '@syncfusion/ej2-react-documenteditor';
+import { TitleBar } from './title-bar';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkFhUX1cdXVRTmBeUUc=');
+
+let data = {
+    "sections": [{
+        "blocks": [{
+            "paragraphFormat": {
+                "styleName": "Normal"
+            },
+            "inlines": [{
+                "text": "TEST TEST",
+                "characterFormat": {
+                    "fontSize": 18.0,
+                    "fontFamily": "Monotype Corsiva"
+                }
+            }]
+        },
+
+        ],
+        "headersFooters": {},
+        "sectionFormat": {
+            "headerDistance": 36.0,
+            "footerDistance": 36.0,
+            "pageWidth": 612.0,
+            "pageHeight": 792.0,
+            "leftMargin": 72.0,
+            "rightMargin": 72.0,
+            "topMargin": 72.0,
+            "bottomMargin": 72.0,
+            "differentFirstPage": false,
+            "differentOddAndEvenPages": false
+        }
+    }],
+    "characterFormat": {
+        "fontFamily": "Times New Roman"
+    },
+    "background": {
+        "color": "#FFFFFFFF"
+    },
+    "styles": [{
+        "type": "Paragraph",
+        "name": "Normal",
+        "next": "Normal",
+        "characterFormat": {
+            "fontSize": 12.0
+        }
+    }, {
+        "type": "Character",
+        "name": "Default Paragraph Font"
+    }]
+};
+
+DocumentEditorContainerComponent.Inject(Toolbar);
+// tslint:disable:max-line-length
+export class CharacterFormatView extends SampleBase {
+    hostUrl = 'https://ej2services.syncfusion.com/production/web-services/';
+    container;
+    titleBar;
+    // onCreated() {
+    //     // To insert text in cursor position
+    //     this.container.documentEditor.editor.insertText('TEST');
+    //     // To select all the content in document
+    //     this.container.documentEditor.selection.selectAll();
+
+    //     // To get the content as text
+    //     let selectedContent = this.container.documentEditor.selection.text;
+    //     console.log("hola")
+    //     console.log(selectedContent)
+    // }
+    
+    onCreated() {
+        // To insert text in cursor position
+        this.container.documentEditor.editor.insertText('Document editor');
+    
+        // To get the content as SFDT
+        let selectedContent = this.container.documentEditor.serialize();
+
+        console.log("test 2")
+        console.log(selectedContent)
+    }
+    rendereComplete() {
+        this.container.serviceUrl = this.hostUrl + 'api/documenteditor/';
+        this.container.documentEditor.pageOutline = '#E0E0E0';
+        this.container.documentEditor.acceptTab = true;
+        this.container.documentEditor.resize();
+        this.titleBar = new TitleBar(
+            document.getElementById('documenteditor_titlebar'),
+            this.container.documentEditor,
+            true
+        );
+        this.onLoadDefault();
+    }
+    render() {
+        return (
+            <div className='control-pane'>
+                <div className='control-section'>
+                    <div
+                        id='documenteditor_titlebar'
+                        className="e-de-ctn-title"
+                    >
+                    </div>
+                    <div id="documenteditor_container_body">
+                        <DocumentEditorContainerComponent
+                            id="container"
+                            ref={(scope) => { this.container = scope; }}
+                            style={{ 'display': 'block' }}
+                            width={'90%'}
+                            height={'590px'}
+                            serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/"
+                            enableToolbar={true}
+                            locale='en-US'
+                            created={this.onCreated.bind(this)}
+                        />
+                    </div>
+                </div>
+                <script>
+                    {window.onbeforeunload = function () {
+                        return 'Want to save your changes?';
+                    }}
+                </script>
+            </div>);
+    }
+    onLoadDefault = () => {
+        // tslint:disable
+        data  
+        this.container.documentEditor.open(JSON.stringify(data));
+        this.container.documentEditor.documentName = 'Character Formatting';
+        this.titleBar.updateDocumentTitle();
+        this.container.documentChange = () => {
+            this.titleBar.updateDocumentTitle();
+            this.container.documentEditor.focusIn();
+        };
+    };
+}
+
+render(<CharacterFormatView />, document.getElementById('sample'));
---
+++ src/vista.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CharacterFormatView } from './index';
+
+let b = CharacterFormatView();
+let c = b.onCreate.
+const vista = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default vista
